c18417e6d84c931c0aac26e70f880393c53f7edd
Simon Mazur <semmaz.box@gmail.com>
2021-11-22 00:53:17 +0200

    Update guestbook.js

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://27d31e05.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
